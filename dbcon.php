599bb6d2ababaac644eed75a85cb5e3783a4b220
ma-cromax <55255012+ma-cromax@users.noreply.github.com>
2019-09-19 09:55:06 +0100

    Add files via upload

--- dbcon.php
+++ dbcon.php
@@ -13,3 +13,23 @@ $con = mysqli_connect('localhost','root','');
 mysqli_select_db($con,'slack')or die(mysqli_error());
 
 ?>
+
+<?php
+/*
+$host		= "localhost"; // Use Local Host Only      
+$username	= "id10928026_root";
+$password	= "12345"; 
+$db_name	= "id10928026_slack"; 
+ 
+$link = mysqli_connect($host, $username, $password, "team_elite");
+ 
+if (!$link) {
+ echo "Connection Error". PHP_EOL;
+ echo "Error Code: ". mysqli_connect_errno().PHP_EOL;
+ echo "Error: Description".mysqli_connect_error().PHP_EOL;
+ exit;
+}
+//echo "Connection to 000webhost Successfull";
+mysqli_select_db($link,'id10928026_slack')or die(mysqli_error());
+*/
+?>
